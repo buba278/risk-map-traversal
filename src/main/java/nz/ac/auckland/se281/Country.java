9b3beba224afb34a70006c69e1c6d55e2051ab85
buba278 <chrismotta2005@gmail.com>
2024-05-24 11:23:05 +1200

    java docs written for the country class

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -1,24 +1,44 @@
 package nz.ac.auckland.se281;
 
+/**
+ * This interface sets the foundation for bot functionality to play it's turn
+ * and select it's strategy.
+ */
 public class Country {
   private String name;
   private String continent;
   private int tax;
 
+  /**
+   * This is the constructor for initialising a country with its attributes.
+
+   * @param name the name of the country.
+   * @param continent the continent the country resides in.
+   * @param tax the amount of tax it costs to traverse the country.
+   */
   public Country(String name, String continent, String tax) {
     this.name = name;
     this.continent = continent;
     this.tax = Integer.parseInt(tax);
   }
 
+  /**
+   * A getter function for returning the continent.
+   */
   public String getContinent() {
     return this.continent;
   }
 
+  /**
+   * A getter function for returning the tax.
+   */
   public int getTax() {
     return this.tax;
   }
 
+  /**
+   * A getter function for returning the name of the country.
+   */
   public String getName() {
     return this.name;
   }
