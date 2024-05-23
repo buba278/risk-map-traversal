462afa17490597cbbbdd0866384d593c5a13f6fd
buba278 <chrismotta2005@gmail.com>
2024-05-24 11:25:19 +1200

    java docs written for the custom exception

--- src/main/java/nz/ac/auckland/se281/CountryNotFoundException.java
+++ src/main/java/nz/ac/auckland/se281/CountryNotFoundException.java
@@ -1,6 +1,14 @@
 package nz.ac.auckland.se281;
 
+/**
+ * An exception for when the country prompted does not exist.
+ */
 public class CountryNotFoundException extends RuntimeException {
+  /**
+   * Constructor for the exception passing its message.
+
+   * @param country the given country that couldnt be found.
+   */
   public CountryNotFoundException(String country) {
     super(MessageCli.INVALID_COUNTRY.getMessage(country));
   }
